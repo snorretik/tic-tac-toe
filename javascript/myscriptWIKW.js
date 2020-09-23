d76cb492969c605eb0bf11a19f02b532090b9c68
Niels Magendans <snorretik@gmail.com>
2020-09-23 22:05:41 +0200

    Careful start

--- javascript/myscriptWIKW.js
+++ javascript/myscriptWIKW.js
@@ -1,3 +1,44 @@
 (function() {
+    // variables
+    // _private
+    // public
 
+    let _play = false;
+
+    const clickMoves = Array.from(document.querySelectorAll(".vakjes"));
+    clickMoves.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            // weet nog niet...
+        })
+    })
+
+    function firstMove() {
+        // handle the names etc...
+    }
 })();
+
+(function(turn) {
+    // computer AI
+    function wonMessage() {
+        const outputVariable = document.querySelector("#outputString");
+        outputVariable.textContent = "Computer has won!";
+    }
+    return {
+        turn: turn,
+        wonMessage: wonMessage
+    }
+})();
+
+function player(name, sign, turn) {
+    // maybe do some more...
+    function wonMessage(name) {
+        const outputVariable = document.querySelector("#outputString");
+        outputVariable.textContent = `${name}, you have won!`;
+    } 
+    return { 
+        name: name, 
+        sign: sign, 
+        turn: turn,
+        wonMessage: wonMessage
+    }
+}
